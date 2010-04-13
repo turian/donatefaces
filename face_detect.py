7790d392342459eb2aaa20101db9f228ba5cab76
Joseph Turian <turian@gmail.com>
2010-04-13 17:04:38 -0400

    Read a video file, grab a frame

--- face_detect.py
+++ face_detect.py
@@ -8,12 +8,16 @@
 # Usage: python face_detect.py <image_file>
 
 import sys, os
+
+import common.video
+
 from opencv.cv import *
 from opencv.highgui import *
 
 def detectObjects(image):
     """Converts an image to grayscale and prints the locations of any
          faces found"""
+
     grayscale = cvCreateImage(cvSize(image.width, image.height), 8, 1)
     cvCvtColor(image, grayscale, CV_BGR2GRAY)
 
@@ -26,13 +30,34 @@ def detectObjects(image):
     faces = cvHaarDetectObjects(grayscale, cascade, storage, 1.2, 2,
                                                          CV_HAAR_DO_CANNY_PRUNING, cvSize(50,50))
 
+    print dir(faces)
     if faces:
         for f in faces:
             print("[(%d,%d) -> (%d,%d)]" % (f.x, f.y, f.x+f.width, f.y+f.height))
+    return faces
 
 def main():
-    image = cvLoadImage(sys.argv[1]);
-    detectObjects(image)
+    pil_img = common.video.grab_frame(sys.argv[1], framenumber=0)
+
+    # Convert to OpenCV image from PIL image, on disk
+    import tempfile
+    tmp = tempfile.NamedTemporaryFile(suffix=".png")
+    pil_img.save(tmp.name)
+    image = cvLoadImage(tmp.name)
+
+#   TODO: Convert to OpenCV image from PIL image, *IN MEMORY*
+#   http://stackoverflow.com/questions/1650568/how-do-i-create-an-opencv-image-from-a-pil-image
+#    cv_img = cv.CreateImageHeader(pil_img.size, cv.IPL_DEPTH_8U, 3)  # RGB image
+#    cv.SetData(cv_img, pil_img.tostring(), pil_img.size[0]*3)
+
+#    print image.imageSize
+    faces = detectObjects(image)
+
+    if faces:
+        for f in faces:
+            print("[(%d,%d) -> (%d,%d)]" % (f.x, f.y, f.x+f.width, f.y+f.height))
+    
+#    tmp.close()
 
 if __name__ == "__main__":
     main()
