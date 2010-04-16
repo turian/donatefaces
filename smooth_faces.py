365b81904555e26e2832a8952d1f7baf35630bf0
Joseph Turian <turian@gmail.com>
2010-04-16 18:41:30 -0400

    Now draws examples that just fail MAXSQRERR test

--- smooth_faces.py
+++ smooth_faces.py
@@ -17,6 +17,10 @@ import common.video
 
 import numpy
 
+# Maximum squared error between the bounding boxes of subseq faces
+MAXSQRERR = 0.01
+#MAXSQRERR = 0.02 # too high
+
 def facenumpy(face, width, height):
     (x1, y1, x2, y2) = face.bbox
     x1 /= 1. * width
@@ -25,10 +29,11 @@ def facenumpy(face, width, height):
     y2 /= 1. * height
     return numpy.array((x1, y1, x2, y2))
 
-
 def main(facefilename, videofilename):
     faces = Faces("")
     faces.__setstate__(common.json.loadfile(facefilename))
+
+    showedcnt = 0
 
     for i, fil, totframes in common.video.frames(videofilename):
         frame = faces.frames[i]
@@ -39,7 +44,16 @@ def main(facefilename, videofilename):
                 for nextface in faces.frames[i+1]:
                     f2 = facenumpy(nextface, faces.width, faces.height)
 
-                    print numpy.sum(numpy.square(f1-f2)), f1, f2
+                    diff = numpy.sum(numpy.square(f1-f2))
+                    if diff > MAXSQRERR and diff < MAXSQRERR*1.1 and showedcnt < 25:
+                        from PIL import Image, ImageDraw
+                        pil_img = Image.open(fil)
+                        draw = ImageDraw.Draw(pil_img)
+                        face.draw(draw)
+                        nextface.draw(draw, color="green")
+                        pil_img.show()
+                        print diff, f1, f2
+                        showedcnt += 1
 
 
 if __name__ == "__main__":
